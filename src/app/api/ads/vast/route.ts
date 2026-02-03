918f3a57b5e3c0187400cdd4e9c841471d1c5f44
huatao999 <huatao999@outlook.com>
2026-02-03 16:48:33 +0800

    Update route.ts

--- src/app/api/ads/vast/route.ts
+++ src/app/api/ads/vast/route.ts
@@ -1,3 +1,4 @@
+export const dynamic = 'force-dynamic';
 import {getAdConfig} from "@/lib/ads/config";
 import {z} from "zod";
 
@@ -48,3 +49,4 @@ export async function GET(req: Request) {
     return Response.json({error: "Get VAST URL failed", message}, {status: 500});
   }
 }
+
