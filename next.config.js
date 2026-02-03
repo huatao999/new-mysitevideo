6010d95fb8f7850205625e3849a4319627772f3e
huatao999 <huatao999@outlook.com>
2026-02-03 19:19:36 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
-module.exports = {
-  
-}
+// 项目根目录/next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // 保留你原来的所有配置（比如images、env等），只加下面这行
+  transpilePackages: ['next-intl'], // 让Next16兼容next-intl，必加
+  reactStrictMode: true, // 可选，加了更规范，不影响功能
+};
+
+export default nextConfig;
