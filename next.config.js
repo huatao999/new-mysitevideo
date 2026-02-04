fbde90a8bf16873d5436a2c92bbaf93aca2726f2
huatao999 <huatao999@outlook.com>
2026-02-04 23:33:12 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // 仅保留Netlify Next.js运行时必需配置，无任何无效项
-  output: "standalone",
-  reactStrictMode: true
-};
-export default nextConfig;
+  output: 'export',
+  trailing
