6c5a84e09a53de9c3a9326aeb467441412ceefd0
huatao999 <huatao999@outlook.com>
2026-02-04 23:37:32 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  trailing
+  trailingSlash: true,
+  images: {
+    unoptimized: true;
+  }
+}
+
+module.exports = nextConfig
